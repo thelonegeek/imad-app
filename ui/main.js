ea910539d7941a5b6f85a9b235e9e68df0b53124
Vishal Gangwar <vishal.gangwar17@gmail.com>
2017-08-21 13:00:49 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -5,7 +5,7 @@ var img = document.getElementById('yo');
 margin = 0;
 function moveRight(){
     margin = margin + 5;
-    img.style.marginleft = margin + 'px';
+    img.style.marginLeft = margin + 'px';
 }
 
 img.onclick = function () {
