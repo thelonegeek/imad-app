b76a1354a0a3bdd46a5b116b2ea2740986ad6a71
Vishal Gangwar <vishal.gangwar17@gmail.com>
2017-08-21 12:28:40 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -70,20 +70,19 @@ return htmltemplate;
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
+
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
 app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
-
 app.get('/:articleName',function(req, res){
     // articleName = article-one 
     // articles[articleName] == { content of the object of articles
    var articleName = req.params.articleName;
    res.send(createtemplate(articles[articleName]));
 });
-
 
 var port = 80;
 app.listen(port, function () {
