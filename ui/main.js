28f71805aec45285757ae8ca27a4cc529c09aefa
Vishal Gangwar <vishal.gangwar17@gmail.com>
2017-08-21 12:59:51 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -3,7 +3,7 @@ console.log('Loaded!');
 var img = document.getElementById('yo');
 
 margin = 0;
-function moveright(){
+function moveRight(){
     margin = margin + 5;
     img.style.margin = margin + 'px';
 }
