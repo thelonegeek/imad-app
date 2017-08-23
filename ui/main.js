b7cc4f98d29ec7c500871ccf4aa351508c93a682
Vishal Gangwar <vishal.gangwar17@gmail.com>
2017-08-23 17:58:11 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,17 +1,27 @@
 console.log('Loaded!');
-
 // clicked button increment value
 var button = document.getElementById('counter');
-var counter = 0;
+
 button.onclick = function () {
     //make a request to the counter  endpoint
-    
+    var request = new XMLHttpRequest();
     //capture the response and store it in a variable
-    
+    request.onreadystatechange = function() {
+        if(request.readystate === XmlHttpRequest.DONE){
+            // Take Some Action
+            if(request.status === 200){
+                var counter = request.responseText;
+            }
+        }
+        // if not done yet do this....
+        else{
+        }
+    };
     // render the value of counter in the correct span
-    counter = counter + 1;
+   /* counter = counter + 1;
     var span = document.getElementById('count');
-    span.innerHTML = counter.toString();
+    span.innerHTML = counter.toString(); 
+   */
 };
 //animation on the picture 
 var img = document.getElementById('yo');
