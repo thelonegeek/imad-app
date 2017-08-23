82f24ab8182b9bca3034c7fa233c13cfff0f20fe
Vishal Gangwar <vishal.gangwar17@gmail.com>
2017-08-23 19:10:39 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,20 +1,19 @@
-console.log('Loaded!');
-var button = document.getElementById('counter');
-button.onclick = function () {
-    //create a Request 
+function exec(){
     var request = new XMLHttpRequest();
-    //capture the response and store it in a variable
-    request.onreadystatechange = function() {
-        if(request.readystate === XMLHttpRequest.DONE){
-            // Take Some Action
-            if(request.status === 200){
-                var counter = request.responseText;
-                var span = document.getElementById('count');
-                span.innerHTML = counter.toString(); 
-            }
-        }
-};
-    //Make the request
-    request.open('GET','http://vishalgangwar17.imad.hasura-app.io/counter', true);
-    request.send(null);
-};
+    request.onreadystatechange = function() 
+    {
+          if (request.readyState === XMLHttpRequest.DONE) 
+          {
+              //Take Some Action
+              if (request.status === 200) 
+                { 
+                    var counter = request.responseText; 
+                    var span = document.getElementById('count');
+                    span.innerHTML = counter.toString();
+                } 
+          } 
+    }; 
+    request.open("GET", "http://kogam22.imad.hasura-app.io/counter", true);
+    request.send(); 
+    console.log('EXECUTED');
+}
