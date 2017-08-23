3f84648d3fa1bbc2e0cec95238c50a9a3e151014
Vishal Gangwar <vishal.gangwar17@gmail.com>
2017-08-23 18:32:19 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -18,12 +18,3 @@ button.onclick = function () {
     request.open('GET','http://vishalgangwar17.imad.hasura-app.io/counter', true);
     request.send(null);
 };
-//animation on the picture 
-var img = document.getElementById('yo');
-margin = 0;
-function moveRight(){
-    margin = margin + 5;
-    img.style.marginLeft = margin + 'px';}
-img.onclick = function () {
-    var interval = setInterval(moveRight, 50);
-};
