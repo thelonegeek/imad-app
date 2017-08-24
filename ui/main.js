803c04ed4667955204947766ff1c373972814d9b
Vishal Gangwar <vishal.gangwar17@gmail.com>
2017-08-24 19:18:30 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -15,21 +15,39 @@ function exec(){
           } 
     }; 
     //MAKE THE REQUEST 
-    request.open("GET", "http://vishalgangwar17.imad.hasura-app.io/counter", true);
+    request.open('GET', 'http://vishalgangwar17.imad.hasura-app.io/counter', true);
     request.send(); 
     console.log('EXECUTED');
 }
-var nameInput = document.getElementById('name');
-var name = nameInput.value;
+// submit button
 var submit = document.getElementById('submit_button');
 submit.onclick = function(){
+    // CREATE THE REQUEST
+    var request = new XMLHttpRequest();
+    request.onreadystatechange = function() 
+    {
+          if (request.readyState === XMLHttpRequest.DONE) 
+          {
+              //Take Some Action
+              if (request.status === 200) 
+                { 
+                    //CAPTURE A LIST OF NAME AND RENDER IT AS LIST
+                        var names = reequest.responseText;
+                        name = JSON.parse.(names);
+                        var list = '';
+                        for( var i=0; i<names.length; i++){
+                            list += '<li>' + names[i] + '</li>';
+                        }
+                        var ul = document.getElementById('namelist');
+                        ul.innerHTML = list;
     
-    var names = ['name1','name2','name3', 'name4'];
-    var list = '';
-    for( var i=0; i<names.length; i++){
-        list += '<li>' + names[i] + '</li>';
-    }
-    var ul = document.getElementById('namelist');
-    ul.innerHTML = list;
-    //CAPTURE A LIST OF NAME AND RENDER IT AS LIST
+                } 
+          } 
+    }; 
+    //MAKE THE REQUEST 
+    var nameInput = document.getElementById('name');
+    var name = nameInput.value;
+    request.open('GET', 'http://vishalgangwar17.imad.hasura-app.io/submit-name=' + name, true);
+    request.send(); 
+    
 };
