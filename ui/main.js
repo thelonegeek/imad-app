254cd97d7cefe10ce9763618e335a2d6d9a8258c
Vishal Gangwar <vishal.gangwar17@gmail.com>
2017-08-24 18:03:36 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,4 +1,5 @@
 function exec(){
+    // CREATE THE REQUEST
     var request = new XMLHttpRequest();
     request.onreadystatechange = function() 
     {
@@ -13,7 +14,22 @@ function exec(){
                 } 
           } 
     }; 
+    //MAKE THE REQUEST 
     request.open("GET", "http://vishalgangwar17.imad.hasura-app.io/counter", true);
     request.send(); 
     console.log('EXECUTED');
 }
+var nameInput = document.getElementById('name');
+var name = nameInput.value;
+var submit = document.getElementById('submit_button');
+submit.onclick = function(){
+    
+    var names = ['name1','name2','name3', 'name4'];
+    var list = '';
+    for( var i=0; i<names.length; i++){
+        list += '<li>' + names[i] + '</li>';
+    }
+    var ul = document.getElementById('namelist');
+    ul.innerHTML = list;
+    //CAPTURE A LIST OF NAME AND RENDER IT AS LIST
+};
