f366f43a83a1b8fbb3005c57cc9c699f1dd866f4
Vishal Gangwar <vishal.gangwar17@gmail.com>
2017-08-24 19:36:19 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -61,7 +61,7 @@ function createtemplate (data){
         <div>
             ${content}
         </div>
-        <textarea rows="5" cols="20" name="comment"> </textarea>
+        <textarea rows="5" cols="4" name="comment"> </textarea>
         </div>
         
     </body>
