b0a4c170eae7fd36bfa3bb0180a2265099d12844
Vishal Gangwar <vishal.gangwar17@gmail.com>
2017-08-24 18:58:45 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -73,6 +73,7 @@ app.get('/counter', function(req, res){
     counter = counter + 1;
     res.send(counter.toString());
 });
+
 var names = [];
 app.get('/submit-button'), function(req,res){
     //get the name from the requesst
@@ -81,9 +82,11 @@ app.get('/submit-button'), function(req,res){
     // JSON :JAVASCRIPT OBJECT INTO STRONG
     res.send(JSON.stringify(names));
 }
+
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
+
 app.get('/favicon.ico', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'favicon.ico'));
 });
@@ -94,12 +97,15 @@ app.get('/:articleName',function(req, res){
    var articleName = req.params.articleName;
    res.send(createtemplate(articles[articleName]));
 });
+
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
+
 app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
+
 app.get('/ui/main.js' , function(req, res) {
     res.sendFile(path.join(__dirname,'ui','main.js'));
 });
